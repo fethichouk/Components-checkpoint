f62c8c685fd196269094f11c40654ba838c3d0ac
Fethi Chouk <choukfethi@outlook.com>
2022-05-09 00:49:45 -0900

    profile

---
+++ src/components/profile/Adress.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Adress() {
+  return (
+    <div>
+   <h3>Adress :</h3>   449 Dayna Park Bartholomebury
+    </div>
+  )
+}
+
+export default Adress
---
+++ src/components/profile/FullName.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function fullName() {
+  return (
+    <div>
+     <h3>FullName:</h3>   Jhon Dhoe
+    </div>
+  )
+}
+
+export default fullName
